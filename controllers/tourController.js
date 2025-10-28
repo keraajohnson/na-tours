1e51e0762d5cd50b1316c1ec325de602661130c3
Kera Johnson <kera@applydigital.co>
2025-10-28 14:29:11 -0400

    update controllers + model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -62,24 +62,43 @@ exports.getTour = async (req, res) => {
   }
 };
 
-// update tour (missing most functionality)
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+// update tour
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-// delete tour (missing most functionality)
-exports.deleteTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Deleted tour here>',
-    },
-  });
+// delete tour
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // graveyard
--- models/tourModel.js
+++ models/tourModel.js
@@ -7,13 +7,38 @@ const toursSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size'],
+  },
+  difficulty: {
+    type: string,
+    required: [true, 'A tour must have a difficulty rating'],
+  },
   rating: {
     type: Number,
     default: 4.5,
   },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
   },
 });
 
